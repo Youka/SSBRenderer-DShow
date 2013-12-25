4b0dacb96c396a9ddb65d14189349d9367ef744c
Youka <spanknebel.borken@t-online.de>
2013-12-25 06:17:34 +0100

    added BaseClasses for DirectShow development

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,92 @@ Permission is granted to anyone to use this software for any purpose, including 
     This notice may not be removed or altered from any source distribution.
 */
 
-#include <windows.h>
+// Include windows 'Globally Unique Identifier'
+#include <initguid.h>
+#include <CGuid.h>
+// Include windows streams
+#include <streams.h>
 
-BOOL APIENTRY DllMain(HANDLE, DWORD, LPVOID){
-    // No errors
-    return TRUE;
+// DLL instance getter for VC compilers
+extern "C" IMAGE_DOS_HEADER __ImageBase;
+#define DLL_INSTANCE reinterpret_cast<HINSTANCE>(&__ImageBase)
+
+
+// Filter pins
+const AMOVIESETUP_MEDIATYPE sudPinTypes[] =
+{
+	// Support RGB colorspace
+	{
+		&MEDIATYPE_Video,
+		&MEDIASUBTYPE_RGB24
+	},
+	// Support RGBA colorspace
+	{
+		&MEDIATYPE_Video,
+		&MEDIASUBTYPE_RGB32
+	}
+};
+
+const AMOVIESETUP_PIN sudpPins[] =
+{
+	{ L"Input",             // Pin string name
+		FALSE,                // Is it rendered
+		FALSE,                // Is it an output
+		FALSE,                // Are we allowed none
+		FALSE,                // And allowed many
+		&CLSID_NULL,          // Connects to filter
+		NULL,                 // Connects to pin
+		2,                    // Number of media types
+		sudPinTypes          // Media types
+	},
+	{ L"Output",            // Pin string name
+		FALSE,                // Is it rendered
+		TRUE,                 // Is it an output
+		FALSE,                // Are we allowed none
+		FALSE,                // And allowed many
+		&CLSID_NULL,          // Connects to filter
+		NULL,                 // Connects to pin
+		2,                    // Number of media types
+		sudPinTypes          // Media types
+	}
+};
+
+// Filter setup
+const AMOVIESETUP_FILTER sudFilter =
+{
+	&CLSID_LVSVideoFilter,         // Filter CLSID
+	FILTER_NAMEW,       // Filter name
+	MERIT_NORMAL,       // Filter merit
+	2,                      // Number of pins
+	sudpPins                // Pin information
+};
+
+// Filter definition as COM objects
+CFactoryTemplate g_Templates[] = {
+	{
+		FILTER_NAMEW,	// Filter name
+		&CLSID_LVSVideoFilter,		// Filter CLSID
+		LVSVideoFilter::CreateInstance,	// Filter instance creation
+		NULL,		// Init routine
+		&sudFilter		// Filter setup
+	}
+};
+int g_cTemplates = sizeof(g_Templates) / sizeof(CFactoryTemplate);	// Number of COM objects
+
+// Register filter to server
+STDAPI DllRegisterServer(){
+    return AMovieDllRegisterServer2( TRUE );
+}
+
+// Unregister filter from server
+STDAPI DllUnregisterServer(){
+    return AMovieDllRegisterServer2( FALSE );
+}
+
+// Further DLL initializations for DirectShow
+extern "C" BOOL WINAPI DllEntryPoint(HINSTANCE, ULONG, LPVOID);
+
+// DLL entry point
+BOOL APIENTRY DllMain(HANDLE module, DWORD reason, LPVOID reserved){
+	return DllEntryPoint(reinterpret_cast<HINSTANCE>(module), reason, reserved);
 }
