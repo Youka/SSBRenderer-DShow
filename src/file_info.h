318494e9fd43f64b466ad8dcebba667475c4cd42
Youka <spanknebel.borken@t-online.de>
2013-12-25 07:23:35 +0100

    added further resources (GUIDs, Logo, FileVersion)

---
+++ src/file_info.h
@@ -0,0 +1,28 @@
+/*
+Project: SSBRenderer-DShow
+File: file_info.h
+
+Copyright (c) 2013, Christoph "Youka" Spanknebel
+
+This software is provided 'as-is', without any express or implied warranty. In no event will the authors be held liable for any damages arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose, including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
+
+    The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the	product documentation would be appreciated but is not required.
+    Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+    This notice may not be removed or altered from any source distribution.
+*/
+
+#pragma once
+
+#define FILTER_NAME "SSBRenderer-DShow"
+#define FILTER_NAMEW L"SSBRenderer-DShow"
+#define FILTER_VERSION 0,0,1,0
+#define FILTER_VERSION_STRING "0.0.1"
+#define FILTER_DESC "SSBRenderer for DirectShow."
+#define FILTER_AUTHOR "Youka"
+#define FILTER_COPYRIGHT FILTER_AUTHOR ", © 2013"
+#define FILTER_FILE_NAME FILTER_NAME ".dll"
+#define FILTER_LANGUAGE 0x409,1252	// English
+
+#define FILTER_LOGO 101
--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ This software is provided 'as-is', without any express or implied warranty. In n
 
 Permission is granted to anyone to use this software for any purpose, including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
 
-    The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+    The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the	product documentation would be appreciated but is not required.
     Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
     This notice may not be removed or altered from any source distribution.
 */
@@ -18,10 +18,37 @@ Permission is granted to anyone to use this software for any purpose, including 
 #include <CGuid.h>
 // Include windows streams
 #include <streams.h>
+// File informations
+#include "file_info.h"
+// Include other necessary classes
+#include <string>
 
 // DLL instance getter for VC compilers
 extern "C" IMAGE_DOS_HEADER __ImageBase;
 #define DLL_INSTANCE reinterpret_cast<HINSTANCE>(&__ImageBase)
+
+// GUIDs for filter
+// {637A5671-7D1B-4b0c-8DB8-2E90BF253D38}
+DEFINE_GUID(CLSID_SSBRenderer,
+0x637a5671, 0x7d1b, 0x4b0c, 0x8d, 0xb8, 0x2e, 0x90, 0xbf, 0x25, 0x3d, 0x38);
+
+// {FD7CD574-A481-4626-9816-2F30347009B7}
+DEFINE_GUID(IID_ISSBRendererConfig,
+0xfd7cd574, 0xa481, 0x4626, 0x98, 0x16, 0x2f, 0x30, 0x34, 0x70, 0x9, 0xb7);
+
+// Filter configuration interface
+interface ISSBRendererConfig : public IUnknown{
+	virtual std::string GetFile() = 0;
+	virtual void SetFile(std::string) = 0;
+};
+
+
+
+
+
+
+
+
 
 
 // Filter pins
@@ -66,7 +93,7 @@ const AMOVIESETUP_PIN sudpPins[] =
 // Filter setup
 const AMOVIESETUP_FILTER sudFilter =
 {
-	&CLSID_LVSVideoFilter,         // Filter CLSID
+	&CLSID_SSBRenderer,         // Filter CLSID
 	FILTER_NAMEW,       // Filter name
 	MERIT_NORMAL,       // Filter merit
 	2,                      // Number of pins
@@ -77,8 +104,8 @@ const AMOVIESETUP_FILTER sudFilter =
 CFactoryTemplate g_Templates[] = {
 	{
 		FILTER_NAMEW,	// Filter name
-		&CLSID_LVSVideoFilter,		// Filter CLSID
-		LVSVideoFilter::CreateInstance,	// Filter instance creation
+		&CLSID_SSBRenderer,		// Filter CLSID
+		/*TODO*/NULL,	// Filter instance creation
 		NULL,		// Init routine
 		&sudFilter		// Filter setup
 	}
